58104142e178177d2012f4572226c4c8738b5c5c
sendy452 <sendyivenyulian452@gmail.com>
2023-10-26 14:32:59 +0700

    set session on topbar

--- resources/views/backend/layouts/header.blade.php
+++ resources/views/backend/layouts/header.blade.php
@@ -169,12 +169,12 @@
 
         <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
           <img src="{{asset('backend/img/profile-img.jpg')}}" alt="Profile" class="rounded-circle">
-          <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+          <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->username }}</span>
         </a><!-- End Profile Iamge Icon -->
 
         <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
           <li class="dropdown-header">
-            <h6>Kevin Anderson</h6>
+            <h6>{{ Auth::user()->name }}</h6>
             <span>Web Designer</span>
           </li>
           <li>
@@ -192,7 +192,7 @@
           </li>
 
           <li>
-            <a class="dropdown-item d-flex align-items-center" href="{{route('logout')}}">
+            <a class="dropdown-item d-flex align-items-center" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
               <i class="bi bi-box-arrow-right"></i>
               <span>Sign Out</span>
             </a>
